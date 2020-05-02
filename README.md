d0d891b76515fe0b540e95ba4a010e5f6e0b41d1
Sujit <xsujit@gmail.com>
2020-05-02 18:25:49 +0100

    Added first test with rest-assured

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rest-assured-demo
 Few examples for using rest-assured
+
+Pre requisite
+1. Wire-mock standalone
+
+Steps to run
+1. Wire-mock creates a mappings folder, place the sample.json provided inside this folder.
+2. Run wire-mock, now wire-mock will respond with the sample json for matching request.
+3. Run wire-mock For e.g. java -jar wiremock-jre8-standalone-2.26.3.jar
+4. Run the test
--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,9 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class ApiTest {
 
     @Test
     public void assertGetResponse() {
-
+        RestAssured.baseURI = "http://localhost:8080";
+        given()
+                .request()
+                .with()
+                .contentType(ContentType.JSON)
+                .log().uri()
+                .when()
+                .get("/test")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("productName", is("Corn flakes"))
+                .body("processIdentifier", is("1212454588"))
+                .rootPath("parameters")
+                .body("productLine", is("FMCG"))
+                .appendRootPath("producers")
+                .body("Kellogg.size()", is(2))
+                .body("Nestle.size()", is(2))
+                .body("Kellogg.stock", is(true))
+                .body("Kellogg.additionalInfo", is("abc-01"))
+                .detachRootPath("producers")
+                .body("Grains.size()", is(2))
+                .body("Grains[0]", hasKey("attribute"))
+                .body("Grains.find{it.attribute == \"Organic available\"}.value", is(true))
+                .appendRootPath("Preservatives")
+                .body("attribute", hasItems("Preservative A", "Preservative B"));
     }
 }
