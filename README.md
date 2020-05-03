bff357bf51a4661cbf250e5c680de68ffb56973c
Sujit <xsujit@gmail.com>
2020-05-03 21:48:28 +0100

    Added http post and schema validation test

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Pre requisite
 1. Wire-mock standalone
 
 Steps to run
-1. Wire-mock creates a mappings folder, place the sample.json provided inside this folder.
-2. Run wire-mock, now wire-mock will respond with the sample json for matching request.
+1. Wire-mock creates a mappings folder, place the wire-mock-mapping.json provided inside this folder.
+2. Run wire-mock, now wire-mock will respond to matching request.
 3. Run wire-mock For e.g. java -jar wiremock-jre8-standalone-2.26.3.jar
 4. Run the test
--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,24 +1,30 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 public class ApiTest {
 
+    @BeforeTest
+    public void setup() {
+        RestAssured.baseURI = "http://localhost:8080";
+    }
+
     @Test
     public void assertGetResponse() {
-        RestAssured.baseURI = "http://localhost:8080";
         given()
-                .request()
-                .with()
                 .contentType(ContentType.JSON)
                 .log().uri()
                 .when()
                 .get("/test")
                 .then()
+                .log()
+                .ifValidationFails()
                 .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body("productName", is("Corn flakes"))
@@ -26,15 +32,45 @@ public class ApiTest {
                 .rootPath("parameters")
                 .body("productLine", is("FMCG"))
                 .appendRootPath("producers")
+                .body("size()", is(2))
                 .body("Kellogg.size()", is(2))
                 .body("Nestle.size()", is(2))
                 .body("Kellogg.stock", is(true))
                 .body("Kellogg.additionalInfo", is("abc-01"))
                 .detachRootPath("producers")
+                // .body("Grains.findAll{true}", everyItem(hasItem("true")))
                 .body("Grains.size()", is(2))
                 .body("Grains[0]", hasKey("attribute"))
                 .body("Grains.find{it.attribute == \"Organic available\"}.value", is(true))
                 .appendRootPath("Preservatives")
                 .body("attribute", hasItems("Preservative A", "Preservative B"));
+    }
+
+    @Test
+    public void schemaTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .log().uri()
+                .when()
+                .get("/schema")
+                .then()
+                .assertThat()
+                .body(matchesJsonSchemaInClasspath("schema.json"));
+    }
+
+    @Test
+    public void postTest() {
+        String payload = "{\"Title\": \"Senior Developer\"}";
+        given()
+                .body(payload)
+                .contentType(ContentType.JSON)
+                .log().body().log().uri()
+                .when()
+                .post("/send")
+                .then()
+                .log().body()
+                .and()
+                .assertThat()
+                .statusCode(200);
     }
 }
